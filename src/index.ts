65216f7ab46e6fca2e16b0d3f090bcf1c9997f18
unknown <mbellman@nwe.com>
2022-10-13 10:54:30 -0500

    pane carousel enhancements

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,17 @@ class AppState {
   public degrees = 0;
 }
 
-function update(dt: number, state: AppState): void {
-  state.degrees += 50 * dt;
-
-  state.carousel.revolve(state.degrees);
-}
-
 function main(): void {
   const state = new AppState();
+  let index = 0;
 
-  let time = Date.now();
-
-  function frame() {
-    const delta = (Date.now() - time) / 1000;
-
-    update(delta, state);
-
-    time = Date.now();
-  
-    requestAnimationFrame(frame);
-  }
-
-  frame();
+  document.body.addEventListener('keydown', e => {
+    if (e.key === 'ArrowRight') {
+      state.carousel.focusByIndex(++index);
+    } else if (e.key === 'ArrowLeft') {
+      state.carousel.focusByIndex(--index);
+    }
+  })
 }
 
 main();
--- src/ui.ts
+++ src/ui.ts
@@ -15,6 +15,14 @@ function createPaneRoot(): HTMLDivElement {
   return pane;
 }
 
+function lerp(a: number, b: number, alpha: number): number {
+  return a + (b - a) * alpha;
+}
+
+function mod(a: number, m: number): number {
+  return ((a % m) + m) % m;
+}
+
 export class Pane {
   private element = createPaneRoot();
 
@@ -43,13 +51,13 @@ export class Pane {
 export class PaneCarousel {
   private root = document.createElement('div');
   private panes: Pane[] = [];
+  private rotation = 0;
+  private currentIndex = 0;
+  private nextAnimationFrame: number = null;
 
   public constructor(total: number, width: number) {
     for (let i = 0; i < total; i++) {
       const pane = new Pane(this.root);
-      const rotation = (i / total) * 360;
-
-      pane.revolve(rotation);
 
       this.panes.push(pane);
     }
@@ -58,13 +66,23 @@ export class PaneCarousel {
     this.root.style.width = `${width}px`;
 
     document.body.appendChild(this.root);
+
+    this.revolve(0);
   }
 
   public focusByIndex(index: number): void {
+    this.currentIndex = mod(index, this.panes.length);
 
+    console.log(this.currentIndex);
+
+    this.revolveToTargetRotation();
   }
 
-  public revolve(degrees: number): void {
+  private get targetRotation(): number {
+    return (this.currentIndex / this.panes.length) * 360;
+  }
+
+  private revolve(degrees: number): void {
     const total = this.panes.length;
 
     for (let i = 0; i < total; i++) {
@@ -72,5 +90,18 @@ export class PaneCarousel {
 
       this.panes[i].revolve(rotation);
     }
+  }
+
+  private revolveToTargetRotation(): void {
+    if (Math.abs(this.rotation - this.targetRotation) < 0.1) {
+      this.rotation = this.targetRotation;
+    } else {
+      window.cancelAnimationFrame(this.nextAnimationFrame);
+
+      this.rotation = lerp(this.rotation, this.targetRotation, 0.1);
+      this.nextAnimationFrame = window.requestAnimationFrame(() => this.revolveToTargetRotation());
+    }
+
+    this.revolve(this.rotation)
   }
 }
