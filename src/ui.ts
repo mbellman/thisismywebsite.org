2abbbe0f4639ff8972bd8fe0959461fca32cb599
unknown <mbellman@nwe.com>
2022-10-19 10:28:34 -0500

    particle behavior tweaks

--- src/ui.ts
+++ src/ui.ts
@@ -174,15 +174,21 @@ export class Particles {
 
     for (let i = 0; i < this.particles.length; i++) {
       const particle = this.particles[i];
+      
+      const baseSize = 5 + Math.sin(i * 1.8) * 3;
+      const speedFactor = 0.6 + 0.4 * (baseSize / 8);
+      const ySpeed = (0.1 + Math.sin(i * 1.7) * 0.02) * speedFactor;
 
       const startX = 600 + Math.sin(i * 1.1) * 300;
       const startY = 300 + Math.cos(i * 2.3) * window.innerHeight;
-      const ySpeed = 0.1 + Math.sin(i * 1.7) * 0.02;
       const x = startX + Math.sin(t + i * 1.3) * 30;
       const y = mod(startY - Date.now() * ySpeed, window.innerHeight);
 
-      const diameter = 5 + Math.sin(t + i) * 3;
-      const color = toRgb(multiply(baseColor, 0.5 + y / window.innerHeight));
+      const heightRatio = y / window.innerHeight;
+      const decay = 1 + heightRatio;
+      const oscillation = 0.8 + Math.sin(i) * 0.2;
+      const diameter = baseSize * decay * oscillation;
+      const color = toRgb(multiply(baseColor, 0.5 + heightRatio * oscillation));
 
       particle.style.left = `${x}px`;
       particle.style.top = `${y}px`;
@@ -190,6 +196,7 @@ export class Particles {
       particle.style.height = `${diameter}px`;
       particle.style.opacity = `${diameter / 5}`;
       particle.style.backgroundColor = `${color}`;
+      particle.style.zIndex = `${Math.round(diameter)}`;
     }
 
     window.requestAnimationFrame(() => this.updateParticles());
